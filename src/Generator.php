ccd8657257c461326b1821cbebdb4034cd4e4ab7
vgalitsky <concept.galitsky@gmail.com>
2024-01-13 00:36:49 +0200

    v1

---
+++ src/Generator.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+namespace Cl\Generator;
+
+class Generator
+{
+    /**
+     * Creates a generator based on the provided value.
+     *
+     * @param mixed $yield The value to include in the generator.
+     * 
+     * @return \Generator
+     */
+    public static function yield(mixed $yield): \Generator
+    {
+        /**
+         * If the provided value is a generator, use the 'from' method for optimal generator creation.
+         * Otherwise, simply include the provided value in the generator.
+         */
+        if ($yield instanceof \Generator) {
+            yield from $yield;
+        } else {
+            yield $yield;
+        }
+    }
+}
---
+++ test/GeneratorTest.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Cl\Generator\Test;
+
+use PHPUnit\Framework\TestCase;
+use Cl\Generator\Generator;
+
+/**
+ * @covers Cl\Generator\Generator
+ */
+class GeneratorTest extends TestCase
+{
+    public function testYieldWithValue()
+    {
+        $value = 'test';
+        $generator = Generator::yield($value);
+
+        $this->assertInstanceOf(\Generator::class, $generator);
+
+        $result = iterator_to_array($generator);
+        $this->assertEquals([$value], $result);
+    }
+
+    public function testYieldWithGenerator()
+    {
+        $innerGenerator = (function () {
+            yield 'inner';
+        })();
+
+        $generator = Generator::yield($innerGenerator);
+
+        $this->assertInstanceOf(\Generator::class, $generator);
+
+        $result = iterator_to_array($generator);
+        $this->assertEquals(['inner'], $result);
+    }
+}
